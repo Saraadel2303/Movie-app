6778ec4e0d9008dbac8276033129de78b6214870
Saraadel2303 <Saraadelfarid990@gmail.com>
2026-01-15 22:07:04 +0200

    show all movies

--- src/components/AllMovies.jsx
+++ src/components/AllMovies.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import MovieCard from './MovieCard'
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import axios from "axios";
 
 export default function AllMovies() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/items")
+      .then((res) => {
+        setMovies(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="p-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-items-center">
-        <MovieCard/>
-        <MovieCard/>
-        <MovieCard/>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,34 +1,39 @@
 ﻿import { FaEdit, FaStar, FaTrash } from "react-icons/fa";
 
-function MovieCard() {
+function MovieCard({ movie }) {
   return (
     <div className="bg-purple-950 text-white rounded-md shadow-md w-64 group overflow-hidden">
       <img
-        src="https://via.placeholder.com/260x350"
-        alt="movie"
+        src={movie.image}
+        alt={movie.name}
         className="w-full h-72 object-cover"
       />
 
       <div className="p-4 space-y-2">
-        <h3 className="text-base font-semibold">The Movie Name</h3>
+        <h3 className="text-base font-semibold">{movie.name}</h3>
 
         <div className="flex gap-2 flex-wrap">
-          <span className="text-xs bg-purple-600 px-2 py-1 rounded">Drama</span>
-          <span className="text-xs bg-purple-600 px-2 py-1 rounded">Crime</span>
+          {movie.genres.map((genre, index) => (
+            <span
+              key={index}
+              className="text-xs bg-purple-600 px-2 py-1 rounded"
+            >
+              {genre}
+            </span>
+          ))}
         </div>
-
         <p className="text-sm text-gray-400 line-clamp-2">
-          Movie description goes here and it can be a bit longer than one line.
+          {movie.description}
         </p>
 
         <div className="text-yellow-400 text-sm flex gap-1">
-        <FaStar/>
-        <FaStar/>
-        <FaStar/>
-        <FaStar/>
-        <FaStar/>
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
         </div>
-        
+
         <div className="flex justify-end gap-2 mt-2 opacity-0 group-hover:opacity-100 transition">
           <button className="bg-purple-700 p-2 rounded hover:bg-purple-600 cursor-pointer">
             <FaEdit />
@@ -43,4 +48,3 @@ function MovieCard() {
 }
 
 export default MovieCard;
-
